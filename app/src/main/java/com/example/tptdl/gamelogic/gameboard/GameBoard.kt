75192a60474c6faa2ac3a6f5e072ae01557fb9bb
alejo-marino <tommy230201@gmail.com>
2021-11-04 16:01:40 -0300

    naming fix

--- app/src/main/java/com/example/tptdl/gamelogic/gameboard/GameBoard.kt
+++ app/src/main/java/com/example/tptdl/gamelogic/gameboard/GameBoard.kt
@@ -1,17 +1,17 @@
 package com.example.tptdl.gamelogic.gameboard
 
 class GameBoard(val width : Int, val height : Int) {
-    var myRows : MutableList<Line> = mutableListOf()
+    var myColumns : MutableList<Line> = mutableListOf()
 
     init {
         for (i in 1..width)
-            myRows.add(Line(height))
+            myColumns.add(Line(height))
     }
 
     fun printBoard() {
         for (i in 0 until height) {
             for (j in 0 until height)
-                print((((myRows[i]).getValueAtIndex(j)).getCellValue()).toString() + " | ")
+                print((((myColumns[i]).getValueAtIndex(j)).getCellValue()).toString() + " | ")
             print("\n")
         }
     }
