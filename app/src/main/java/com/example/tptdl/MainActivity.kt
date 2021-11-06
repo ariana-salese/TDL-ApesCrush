d2f7e01db7658de4ddad41c3a617374a739172db
Marcelo Rondan <mrondan@fi.uba.ar>
2021-11-06 17:31:11 -0300

    API Weather con corrutinas y fetchCurrent actualizado

--- app/src/main/java/com/example/tptdl/MainActivity.kt
+++ app/src/main/java/com/example/tptdl/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tptdl.gamelogic.gameboard.GameBoard
 import com.example.tptdl.weatherAPI.Weather
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +16,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val weather =  Weather()
 
-        /*
-        Thread {
-            weather.fetchCurrentWeather()
-        }.start()
-         */ //TODO implementarlo con corutinas
+        GlobalScope.launch{
+            println(weather.fetchCurrent())
+        }
+
+        //TODO implementarlo con corutinas
 
         //val gameBoard: GameBoard = GameBoard(3,3)
         //gameBoard.printBoard()
