4bd2495274bfbb75e0634686fe8f6bdb994bb2d9
GabrielSem <gsemorile@fi.uba.ar>
2021-11-21 16:07:38 -0300

    nuevo tablero (por codigo, no xml)

--- app/src/main/java/com/example/tptdl/LevelActivity.kt
+++ app/src/main/java/com/example/tptdl/LevelActivity.kt
@@ -1,79 +1,81 @@
 package com.example.tptdl
 
-import android.annotation.SuppressLint
+import android.R.attr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
+import android.widget.*
+import androidx.core.view.marginStart
 import com.example.tptdl.gamelogic.MovementsCounter
 import com.example.tptdl.gamelogic.Score
 import com.example.tptdl.gamelogic.gameboard.GameBoard
 import com.example.tptdl.weatherAPI.Normal
 import kotlin.math.roundToInt
+import android.R.attr.button
 
-import androidx.gridlayout.widget.GridLayout
-import androidx.recyclerview.widget.GridLayoutManager
+import android.widget.LinearLayout
+
+
+
 
 class LevelActivity : AppCompatActivity() {
     val height = 9
-    private lateinit var gameboard : GameBoard
+    private lateinit var gameboard: GameBoard
+
     //private lateinit var mainBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level)
-        gameboard = GameBoard(height,height, Normal(), Score(), MovementsCounter())
-        var displayMetrics= DisplayMetrics()
+        gameboard = GameBoard(height, height, Normal(), Score(), MovementsCounter())
+        var displayMetrics = DisplayMetrics()
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics)
         val widthScreen = displayMetrics.widthPixels
-        val widthBoard = (widthScreen * 0.8).roundToInt()
+        val widthBoard = (widthScreen * 0.9).roundToInt()
 
-        createBoard(widthBoard)
+        createBoard(height, widthBoard)
 
-    }
-
-    fun clickOnLevelButton(view: View) {
-        println(view.id)
-    }
-
-    fun createBoard(widthBoard : Int) {
-        /*
-        // seteamos grid layout
-        val grid = GridLayoutManager(this, 3)
-
-        // seteamos parametros de gameboard
-        grid.setRowCount(height)
-        grid.setColumnCount(height)
-        grid.getLayoutParams().height = widthBoard
-        val cellWidth = widthBoard / height
-        grid.getLayoutParams().width = widthBoard
-
-
-        for (col in 0 until height) {
-            val imageButton = ImageButton(this)
-            imageButton.id = col
-            imageButton.setLayoutParams(android.view.ViewGroup.LayoutParams(cellWidth, cellWidth))
-            imageButton.maxHeight = cellWidth
-            imageButton.maxWidth = cellWidth
-            imageButton.setImageResource(R.drawable.banana)
-            grid.addView(imageButton)
-
-            for (fil in 0 until height) {
-                val imageButton = ImageButton(this)
-                imageButton.id = fil + 10
-                imageButton.setLayoutParams(android.view.ViewGroup.LayoutParams(cellWidth, cellWidth))
-                imageButton.maxHeight = cellWidth
-                imageButton.maxWidth = cellWidth
-                imageButton.setImageResource(R.drawable.banana)
-                grid.addView(imageButton)
-
-            }
+        fun clickOnLevelButton(view: View) {
+            println(view.id)
         }
-        */
-
     }
 
+    private fun createBoard(height: Int, widthBoard: Int) {
+        val linearLayout = findViewById<LinearLayout>(R.id.gameBoardLayout)
+        val params = linearLayout.layoutParams
+        //params.height = widthBoard
+        params.width = widthBoard
+        linearLayout.layoutParams = params
+        val table = findViewById<TableLayout>(R.id.gameBoardTable)
+        for (row in 0 until height) {
+            //table.setColumnShrinkable(row, true)
+            val currentRow = TableRow(this)
 
+            for (button in 0 until height) {
+                println(button)
+                val currentButton = ImageButton(this)
+                currentButton.setImageResource(R.drawable.orange)
+                currentButton.scaleType = ImageView.ScaleType.FIT_XY
+
+                // you could initialize them here
+                //currentButton.setOnClickListener(listener)
+
+                // you can store them
+                //buttonArray[row][button] = currentButton
+
+                currentRow.addView(currentButton)
+                val params = currentButton.layoutParams
+                params.height = widthBoard / height
+                params.width = widthBoard / height
+                //currentButton.layoutParams = params
+            }
+            // a new row has been constructed -> add to table
+            table.addView(currentRow)
+        }
+// and finally takes that new table and add it to your layout.
+
+
+
+
+    }
 }
-//TODO centrar (gravity), resize imagenes
