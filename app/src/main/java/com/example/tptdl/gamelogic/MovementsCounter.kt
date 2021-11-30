607b36d45e4a100bcef2e62b54d1882696027bae
ariana-salese <asalese@fi.uba.ar>
2021-11-30 16:56:50 -0300

    detalles de UI general

--- app/src/main/java/com/example/tptdl/gamelogic/MovementsCounter.kt
+++ app/src/main/java/com/example/tptdl/gamelogic/MovementsCounter.kt
@@ -1,8 +1,8 @@
 package com.example.tptdl.gamelogic
 
 class MovementsCounter(private var remainingMovements : Int = 20) {
+
     fun checkIfLoss() : Boolean {
-        println("Current movements left: $remainingMovements")
         return this.remainingMovements <= 0
     }
 
